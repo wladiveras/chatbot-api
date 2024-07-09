fe48d90c86332b72e4eb5b511c9a9291cbf2fd01
Wladi Veras <hi@wladi.com.br>
2024-07-09 16:16:37 -0300

    chore: Add evolution_api service to docker-compose.yml

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'evolution_url' => env('EVOLUTION_API_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
