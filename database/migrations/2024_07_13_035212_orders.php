c816fc47abdd04b340cbb7393f98554d001918c5
Wladi Veras <hi@wladi.com.br>
2024-07-13 03:52:21 -0300

    Refactor database migrations to add timestamps to tables

--- database/migrations/2024_07_13_035212_orders.php
+++ database/migrations/2024_07_13_035212_orders.php
@@ -14,25 +14,25 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
-            $table->foreignId('lead_id')->nullable()->index();
+            $table->foreignId('lead_id ')->nullable()->index();
             $table->foreignId('product_id')->nullable()->index();
             $table->foreignId('payment_id')->nullable()->index();
             $table->string('fee')->nullable();
             $table->string('amount')->nullable();
             $table->string('total')->nullable();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
             $table->integer('last_activity')->index();
+            $table->timestamps();
         });
 
         Schema::create('payment_requests', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->foreignId('product_id')->nullable()->index();
+            $table->longText('payload');
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
-            $table->longText('payload');
             $table->integer('last_activity')->index();
+            $table->timestamps();
         });
     }
 
--- database/migrations/2024_07_13_054638_product.php
+++ database/migrations/2024_07_13_054638_product.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->foreignId('user_id')->nullable()->index();
             $table->foreignId('product_id')->nullable()->index();
             $table->foreignId('client_id')->nullable()->index();
+            $table->timestamps();
         });
     }
 
