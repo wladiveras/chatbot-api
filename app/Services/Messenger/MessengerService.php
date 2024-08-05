d762a276223f2d84c4650a717fd8004ce8b771a8
Wladi Veras <hi@wladi.com.br>
2024-08-05 20:28:51 -0300

    feat: Add route for changing flow in MessengerController
    This commit adds a new route `/change-flow/{flow_id}` in the `api.php` file of the `MessengerController`. The route is used to change the selected flow for a messenger connection. This feature allows users to dynamically switch between different conversation flows in the messenger application.
    
    Please note that this commit message does not include any meta information such as issue references, tags, or author names.

--- app/Services/Messenger/MessengerService.php
+++ app/Services/Messenger/MessengerService.php
@@ -20,7 +20,7 @@ class MessengerService extends BaseService
 
     public static function integration(string $provinder): MessengerServiceInterface
     {
-        Log::debug(__CLASS__.'.'.__FUNCTION__.' => running');
+        Log::debug(__CLASS__ . '.' . __FUNCTION__ . ' => running');
 
         return match ($provinder) {
             'whatsapp' => new WhatsappProvinder,
@@ -30,15 +30,15 @@ class MessengerService extends BaseService
 
     public function fetchConnections(): ?stdClass
     {
-        Log::debug(__CLASS__.'.'.__FUNCTION__.' => running');
+        Log::debug(__CLASS__ . '.' . __FUNCTION__ . ' => running');
 
         try {
 
             $connections = $this->connectionRepository->getUserConnections();
 
-            if (! $connections) {
+            if (!$connections) {
                 return $this->error(
-                    path: __CLASS__.'.'.__FUNCTION__,
+                    path: __CLASS__ . '.' . __FUNCTION__,
                     message: 'Não deu certo, não foi possivel trazer as conexões.',
                     code: 400
                 );
@@ -51,7 +51,7 @@ class MessengerService extends BaseService
 
         } catch (\Exception $e) {
             return $this->error(
-                path: __CLASS__.'.'.__FUNCTION__,
+                path: __CLASS__ . '.' . __FUNCTION__,
                 message: $e->getMessage(),
                 code: $e->getCode()
             );
@@ -60,15 +60,15 @@ class MessengerService extends BaseService
 
     public function fetchConnection($id): ?stdClass
     {
-        Log::debug(__CLASS__.'.'.__FUNCTION__.' => running');
+        Log::debug(__CLASS__ . '.' . __FUNCTION__ . ' => running');
 
         try {
 
             $connections = $this->connectionRepository->getUserConnection($id);
 
-            if (! $connections) {
+            if (!$connections) {
                 return $this->error(
-                    path: __CLASS__.'.'.__FUNCTION__,
+                    path: __CLASS__ . '.' . __FUNCTION__,
                     message: 'Não deu certo, não foi possivel trazer as conexões.',
                     code: 400
                 );
@@ -81,7 +81,7 @@ class MessengerService extends BaseService
 
         } catch (\Exception $e) {
             return $this->error(
-                path: __CLASS__.'.'.__FUNCTION__,
+                path: __CLASS__ . '.' . __FUNCTION__,
                 message: $e->getMessage(),
                 code: $e->getCode()
             );
